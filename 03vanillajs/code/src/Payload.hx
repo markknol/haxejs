cf8c93f524c59f157d9df606ce0392cac7f31e8c
Matthijs Kamstra aka [mck] <matthijs@fonkmobile.nl>
2015-09-14 21:19:31 +0200

    Disqus update (I hope)

---
+++ 03vanillajs/code/src/Payload.hx
@@ -0,0 +1,31 @@
+package;
+
+class Payload {
+	
+
+	public var text:String;
+  
+  	private var username:String;
+	private var channel:String;
+	private var icon_url:String;
+	private var icon_emoji:String;
+	// private var attachments:String;
+	private var unfurl_links:String;
+	private var link_names:String;
+
+
+	public function new(message:String):Void
+	{
+	    
+	}
+
+}
+
+/*
+
+typedef Payload = {
+
+
+
+}
+ */
